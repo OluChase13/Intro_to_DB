d244b5b6936dd154ad7551e144612b59def14360
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-10-20 22:28:55 +0100

    task3 file added

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,13 @@
+import mysql.connector
+
+# Replace with your connection details
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    password="yourpassword",
+    database="yourdatabase"
+)
+
+
+
 SHOW TABLES;
