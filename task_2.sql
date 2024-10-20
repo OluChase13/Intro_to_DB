b99a31e18a72b44b2ba7af226ad0f1e5fc094978
Owagbemi Olumide <olumideowagbemi@gmail.com>
2024-10-20 22:10:07 +0100

    task file added

--- task_2.sql
+++ task_2.sql
@@ -28,11 +28,4 @@ CREATE TABLE IF NOT EXISTS Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS Order_details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
-);
+CREATE TABLE IF NOT EXISTS Order_Details", "FOREIGN KEY (order_id) REFERENCES Orders(order_id)", "FOREIGN KEY (book_id) REFERENCES Books(book_id)
